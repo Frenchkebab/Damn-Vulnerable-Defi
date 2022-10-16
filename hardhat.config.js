52a123e177330511cfb00069dd2c1dbff59bfcc4
frenchkebab <tubowangin@gmail.com>
2022-10-16 17:18:58 +0900

    Add dependencies and Changes hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,40 +1,24 @@
-require("dotenv").config();
+require('@nomiclabs/hardhat-waffle');
+require('@openzeppelin/hardhat-upgrades');
+require('hardhat-dependency-compiler');
 
-require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-waffle");
-require("hardhat-gas-reporter");
-require("solidity-coverage");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    hardhat: {
+      allowUnlimitedContractSize: true,
     },
   },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
+  solidity: {
+    compilers: [
+      { version: '0.8.7' },
+      { version: '0.7.6' },
+      { version: '0.6.6' },
+    ],
   },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+  dependencyCompiler: {
+    paths: [
+      '@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol',
+      '@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol',
+    ],
   },
 };
